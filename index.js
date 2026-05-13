6eb4d30d73ee17d82966e28db37f759571a7af23
[shinta9911] <[shintashalsabilla58@gmail.com]>
2026-05-13 01:47:04 +0000

    add missing index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("hitung").addEventListener("click", function () {
+  const angka1 = parseFloat(document.getElementById("angka1").value);
+  const angka2 = parseFloat(document.getElementById("angka2").value);
+
+  const operasi = document.querySelector('input[name="operasi"]:checked').value;
+
+  let hasil = 0;
+
+  if (operasi === "penjumlahan") {
+    hasil = angka1 + angka2;
+  } else if (operasi === "pengurangan") {
+    hasil = angka1 - angka2;
+  } else if (operasi === "perkalian") {
+    hasil = angka1 * angka2;
+  } else if (operasi === "pembagian") {
+    hasil = angka1 / angka2;
+  }
+
+  document.querySelector("h1").innerText = "Hasil: " + hasil;
+});
